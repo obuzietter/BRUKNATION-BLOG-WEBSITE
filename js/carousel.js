a1089520c14d2f9197f442d9dc15b1acaf1637c9
obuzietter <obuyacalvince672@gmail.com>
2023-07-15 21:56:51 +0300

    js carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,22 +1,70 @@
-const carouselContainer = document.querySelector('.carousel-container');
-const slides = document.querySelectorAll('.carousel-slide');
+const carouselContainer = document.querySelector(".carousel-container");
+let slides = document.querySelectorAll(".carousel-slide");
+slideIndex = 0;
+slidesCount = slides.length
+// function nextSlide() {
+//   slides.forEach((slide) => {
+//     for (let slideIndex = 0; slideIndex < slides.length; slideIndex++) {
+//       setTimeout(() => {
+//         slide.style.transform = "translate(-100%)";
+//       }, 1000);
 
-const slideWidth = slides[0].clientWidth; // Assuming all slides have the same width
-let slideIndex = 0;
+//       console.log(slideIndex);
+//     }
+//   });
+// }
+let offset = 0
+let t = 0
+setInterval(() => {
+    slideIndex++
 
-function showSlide() {
-  slides.forEach((slide, index) => {
-    const slideOffset = (index - slideIndex) * slideWidth;
-    slide.style.transform = `translateX(${slideOffset}px)`;
-  });
-}
+    console.log(slideIndex);
+    t = offset
+    if (slideIndex <= slides.length) {
+        offset = offset - 600
+        slides.forEach((slide) => {
+            slide.style.transform = `translate(${offset}px)`;
+        });
+    } 
+    else if (slideIndex > slides.length && slideIndex <= (slides.length * 2)) {
+       
+    
+        slides.forEach((slide) => {
+            offset = t + 600
+            slide.style.transform = `translate(${offset}px)`;
+        })
 
-function nextSlide() {
-  slideIndex++;
-  if (slideIndex >= slides.length) {
-    slideIndex = 0;
-  }
-  showSlide();
-}
 
-// setInterval(nextSlide, 2000);
+    }else{
+        slideIndex = 0
+        
+    }
+}, 1000);
+// const slideWidth = slides[0].clientWidth; // Assuming all slides have the same width
+
+// function showSlide() {
+//   slides.forEach((slide, index) => {
+//     const slideOffset = (index - slideIndex) * slideWidth;
+//     slide.style.transform = `translateX(${slideOffset}px)`;
+//   });
+// }
+
+// function nextSlide() {
+//   slideIndex++;
+
+//   while (slideIndex<slides.length) {
+//     slides.forEach((slide)=>{
+
+//         slide[slideIndex].style.transform = "translate(-100%)"
+//         slideIndex++
+//       })
+//   }
+//    if (slideIndex >= slides.length) {
+//     slideIndex = 0;
+
+//   }
+
+//   showSlide();
+// }
+// console.log(slideLength)
+// setInterval(nextSlide, 1000)
