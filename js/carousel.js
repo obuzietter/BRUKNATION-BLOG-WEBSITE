0cc2a8a0bceea5646f0919939563585d383b4fe2
obuzietter <obuyacalvince672@gmail.com>
2023-07-16 17:24:37 +0300

    Update carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -2,17 +2,7 @@ const carouselContainer = document.querySelector(".carousel-container");
 let slides = document.querySelectorAll(".carousel-slide");
 slideIndex = 0;
 slidesCount = slides.length
-// function nextSlide() {
-//   slides.forEach((slide) => {
-//     for (let slideIndex = 0; slideIndex < slides.length; slideIndex++) {
-//       setTimeout(() => {
-//         slide.style.transform = "translate(-100%)";
-//       }, 1000);
 
-//       console.log(slideIndex);
-//     }
-//   });
-// }
 let offset = 0
 let t = 0
 setInterval(() => {
@@ -40,31 +30,3 @@ setInterval(() => {
         
     }
 }, 1000);
-// const slideWidth = slides[0].clientWidth; // Assuming all slides have the same width
-
-// function showSlide() {
-//   slides.forEach((slide, index) => {
-//     const slideOffset = (index - slideIndex) * slideWidth;
-//     slide.style.transform = `translateX(${slideOffset}px)`;
-//   });
-// }
-
-// function nextSlide() {
-//   slideIndex++;
-
-//   while (slideIndex<slides.length) {
-//     slides.forEach((slide)=>{
-
-//         slide[slideIndex].style.transform = "translate(-100%)"
-//         slideIndex++
-//       })
-//   }
-//    if (slideIndex >= slides.length) {
-//     slideIndex = 0;
-
-//   }
-
-//   showSlide();
-// }
-// console.log(slideLength)
-// setInterval(nextSlide, 1000)
