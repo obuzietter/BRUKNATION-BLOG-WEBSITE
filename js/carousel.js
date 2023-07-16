cccabfa58a0c42a842bc0d512a81fe0f34e7e1f8
obuzietter <obuyacalvince672@gmail.com>
2023-07-16 18:12:17 +0300

    carousel working

--- js/carousel.js
+++ js/carousel.js
@@ -1,32 +1,30 @@
 const carouselContainer = document.querySelector(".carousel-container");
 let slides = document.querySelectorAll(".carousel-slide");
 slideIndex = 0;
-slidesCount = slides.length
+slidesCount = slides.length;
 
-let offset = 0
-let t = 0
+let offset = 0;
+let t = 0;
 setInterval(() => {
-    slideIndex++
-
+    slideIndex++;
+    t = offset;
     console.log(slideIndex);
-    t = offset
-    if (slideIndex <= slides.length) {
-        offset = offset - 600
+    if (slideIndex > (slides.length * 2-2)) {
+        slideIndex = 0;
+        
+    }
+    
+    else if (slideIndex < slides.length) {
+        offset = offset - 600;
         slides.forEach((slide) => {
             slide.style.transform = `translate(${offset}px)`;
         });
-    } 
-    else if (slideIndex > slides.length && slideIndex <= (slides.length * 2)) {
-       
-    
+    } else if (slideIndex >= slides.length && slideIndex <= (slides.length * 2-2)) {
         slides.forEach((slide) => {
-            offset = t + 600
+            offset = t + 600;
             slide.style.transform = `translate(${offset}px)`;
-        })
-
-
-    }else{
-        slideIndex = 0
-        
+        });
     }
 }, 1000);
+
+// (slideIndex >= slides.length && slideIndex < slides.length * 2)
